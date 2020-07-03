51ab42d30e3b19c7f5abfe82bfe2037bd8946988
Cristian Jaldin <cristianjaldin@hotmail.com>
2020-07-02 21:34:20 -0300

    initial commit

---
+++ src/main/java/com/example/demo/sdk/user/UserSdkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.sdk.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSdkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/sdk/user/UserSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.sdk.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
