e25d1b0745dac1c218ac36f98f47f425b106fa12
Cristian Jaldin <cristianjaldin@hotmail.com>
2020-07-12 21:29:52 -0300

    add update

--- src/main/java/com/example/demo/sdk/user/service/UserSdk.java
+++ src/main/java/com/example/demo/sdk/user/service/UserSdk.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.example.demo.dto.user.UserCreateDto;
 import com.example.demo.dto.user.UserDto;
+import com.example.demo.dto.user.UserUpdateDto;
 
 public interface UserSdk {
-	List<UserDto> findAll();
 	UserDto get(String id);
 	UserDto add(UserCreateDto userCreateDto);
+	UserDto update(String id, UserUpdateDto userUpdateDto);
 	void delete(String id);
+	List<UserDto> findAll();
 }
--- src/main/java/com/example/demo/sdk/user/service/UserSdkImpl.java
+++ src/main/java/com/example/demo/sdk/user/service/UserSdkImpl.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import com.example.demo.dto.response.Response;
 import com.example.demo.dto.user.UserCreateDto;
 import com.example.demo.dto.user.UserDto;
+import com.example.demo.dto.user.UserUpdateDto;
 import com.example.demo.sdk.user.provider.UserUrlProvider;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -163,5 +164,51 @@ public class UserSdkImpl implements UserSdk {
 		if (result.getStatusCodeValue() != 204) {
 			throw new HttpClientErrorException(result.getStatusCode());
 		}
+	}
+
+	@Override
+	public UserDto update(String id, UserUpdateDto userUpdateDto) {
+		UserDto userUpdated = null;
+		// Url
+		final String userUrl = userUrlProvider.getUserUrl().concat("/").concat(id);
+		System.out.println(userUrl);
+		// headers
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+
+		// body
+		String body = null;
+		try {
+			body = objectMapper.writeValueAsString(userUpdateDto);
+		} catch (JsonProcessingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		// Request con body + headers
+		HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
+
+		// execute PUT
+		ResponseEntity<String> result = restTemplate.exchange(userUrl, HttpMethod.PUT, requestEntity, String.class);
+
+		System.out.println("result.getStatusCodeValue(): " + result.getStatusCodeValue());
+		System.out.println("result.getBody(): " + result.getBody());
+
+		if (result.getStatusCodeValue() != 200) {
+			throw new HttpClientErrorException(result.getStatusCode());
+		}
+
+		try {
+			Response response = objectMapper.readValue(result.getBody(), new TypeReference<Response>() {
+			});
+			// response.setPayload(objectMapper.convertValue(response.getPayload(), new TypeReference<UserDto>() {}));
+			userUpdated = objectMapper.convertValue(response.getPayload(), new TypeReference<UserDto>() {
+			});
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return userUpdated;
 	}
 }
